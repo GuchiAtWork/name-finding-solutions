48a537b68645da5ec1e7c2368b269a243b25bf27
Keizo Hamaguchi <hamaguke94@gmail.com>
2023-02-02 23:55:46 +0900

    ensuring that the app actually runs

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
+from views.status_view import status_blueprint
+
 app = Flask(__name__)
+app.register_blueprint(status_blueprint)
 
-
-@app.route("/")
-def home():
-    return "Hello World! I'm using Flask."
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
